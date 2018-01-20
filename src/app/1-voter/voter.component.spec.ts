371edb2d428129b32308bb23bcf18ffb5c0333eb
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-01-20 22:33:35 +0100

    testing event binding

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -35,4 +35,11 @@ describe('VoterComponent', () => {
         expect(de.classes['highlighted']).toBeTruthy();
     });
 
+    it('should increase total votes when I click the upvote button', () => {
+        let button = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+        button.triggerEventHandler('click', null);
+
+        expect(component.totalVotes).toBe(1);
+    });
+
 });
